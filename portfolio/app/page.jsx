9673d47340b1a5de692138ef328669fe81d05693
Phú Nguyễn <2159011@itec.hcmus.edu.vn>
2024-10-12 19:22:12 +0700

    complete homepage

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -3,13 +3,14 @@ import { FiDownload } from "react-icons/fi";
 
 import Socials from "@/components/Socials";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Fullstack Developer</span>
             <h1 className="h1">
               Hello I'm <br />{" "}
@@ -40,11 +41,12 @@ const Home = () => {
             </div>
           </div>
 
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
