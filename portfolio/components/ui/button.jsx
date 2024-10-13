37d8cc16b5c75007bdeea36acfb6947d1861e4c0
Phú Nguyễn <2159011@itec.hcmus.edu.vn>
2024-10-13 22:33:24 +0700

    complete contact page

--- portfolio/components/ui/button.jsx
+++ portfolio/components/ui/button.jsx
@@ -9,10 +9,10 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          "bg-accent text-primary hover:bg-accent-hover ",
-          primary: "bg-primary text-white",
-          outline: "border-accent bg-transparent text-accent hover:bg-accent hover:text-primary",
+        default: "bg-accent text-primary hover:bg-accent-hover ",
+        primary: "bg-primary text-white",
+        outline:
+          "border-accent bg-transparent text-accent hover:bg-accent hover:text-primary",
       },
       size: {
         default: "h-[44px] px-6",
