b4519ab1e5570d5ed9f554026cc8595119e42e69
Phú Nguyễn <2159011@itec.hcmus.edu.vn>
2024-10-14 21:00:00 +0700

    Update Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -22,7 +22,7 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="#"
+            src=""
             priority
             quality={100}
             fill
