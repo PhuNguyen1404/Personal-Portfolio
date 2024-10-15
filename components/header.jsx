94cb13b0b783613a0cfd238d91ed8948305c0b40
Phú Nguyễn <117805903+PhuNguyen1404@users.noreply.github.com>
2024-10-15 21:25:34 +0700

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header className="py-8 xl:py-12 text-white">
       <div className="container mx-auto flex justify-between items-center">
         {/* logo */}
-        <Link href=" ">
+        <Link href="/">
           <h1 className="text-4xl font-semibold">
             Phú<span className="text-[#e76f51]">.</span>
           </h1>
